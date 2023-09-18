92da3957728012bebb0dbadea47dec3bfc8bcc45
AchieBee <belza.achieng@student.moringaschool.combelza.achieng@student.moringaschool.com>
2023-09-18 13:01:20 +0300

    complete assignment

--- lib/count_sentences.py
+++ lib/count_sentences.py
@@ -1,4 +1,20 @@
-#!/usr/bin/env python3
-
 class MyString:
-  pass
+    def __init__(self, value=''):
+        if isinstance(value, str):
+            self.value = value
+        else:
+            self.value = ''
+            print("The value must be a string.")
+
+    def is_sentence(self):
+        return self.value.endswith('.')
+
+    def is_question(self):
+        return self.value.endswith('?')
+
+    def is_exclamation(self):
+        return self.value.endswith('!')
+
+    def count_sentences(self):
+        sentences = filter(None, re.split(r'[.!?]', self.value))
+        return len(list(sentences))
